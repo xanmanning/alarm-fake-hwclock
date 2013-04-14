b7270df215b232fd41d9383649afe4f6b5644e56
Xan Manning <xan.manning@gmail.com>
2013-04-14 21:44:38 +0100

    C rewrite

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ alarm-fake-hwclock
 
 **Written by Xan Manning**
 
-Dead simple script for Arch Linux Arm on Raspberry Pi that reproduces the fake-hwclock on Raspbian.
+Dead simple C program for Arch Linux Arm on Raspberry Pi that works like fake-hwclock on Raspbian.
---
+++ fake-hwclock.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <time.h>
+#include <utime.h>
+#include <sys/stat.h>
+
+int main (int argc, char *argv[]) {
+	struct stat temp;
+	struct utimbuf time_update;
+	const char *timefile = argv[0];
+	char date_command[100];
+	char raw_date_command[50] = "date --set=";
+	time_t current_time;
+	time_t time_store;
+
+	current_time = time(NULL);
+	if(current_time == ((time_t) - 1))
+		return 1;
+
+	if(stat(timefile, &temp) < 0)
+		return 1;
+
+	time_store = temp.st_mtime;
+
+	if(time_store > current_time) {
+		time_t timebuffer = time_store;
+		sprintf(date_command, "%s'%s'", raw_date_command, ctime(&timebuffer));
+		system(date_command);
+	} else {
+		time_update.actime = temp.st_atime;
+		time_update.modtime = time(NULL);
+		utime(timefile, &time_update);
+	}
+
+	return 0;
+}
